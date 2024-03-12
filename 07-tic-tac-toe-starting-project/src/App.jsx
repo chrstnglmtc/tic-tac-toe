07c317ecd4ffd65f5ce01a8aeeab7fc9b47a8b74
chrstnglmtc <christinegulmatico@gmail.com>
2024-03-12 09:49:25 +0800

    added gameboard for tic tac toe

---
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -0,0 +1,29 @@
+import Player from "./components/Player"
+import GameBoard from "./components/GameBoard";
+import { useState } from "react";
+import Log from "./components/Log";
+
+function App() {
+  const [ activePlayer, setActivePlayer ] = useState('X');
+  const [ gameTurns, setGameTurns ] = useState([]);
+
+  function handleSelectSquare() {
+    setActivePlayer((curActivePlayer) => curActivePlayer ==='X' ? 'O' : 'X');
+    setGameTurns(); 
+  }
+
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          <Player initialName="Player 1" symbol="X"highlight-player isActive={activePlayer === 'X'}/>
+          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
+        </ol>
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+      </div>
+      <Log />
+    </main>
+  )
+}
+
+export default App
